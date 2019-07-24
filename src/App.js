a6a3ce66f1abbd82a24cef9473aa5c0cf7ff3ede
Bruno Gustavo <brugucolosil95@gmail.com>
2019-07-24 11:39:21 -0300

    configurando reactotron e implementando redux/redux-saga

--- src/App.js
+++ src/App.js
@@ -1,31 +1,37 @@
 import React, { Fragment } from 'react';
-import GlobalStyle from './styles/global';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+
+import './config/reactotronConfig';
 
 import Sidebar from './components/sidebar';
 import Player from './components/player';
 import Header from './components/header';
 
 import { Wrapper, Container, Content } from './styles/components';
+import GlobalStyle from './styles/global';
 
 import Routes from './routes';
+import store from './store';
 
 function App() {
   return (
     <Fragment>
-      <BrowserRouter>
-        <Wrapper>
-          <Container>
-            <Sidebar />
-            <Content>
-              <Header />
-              <Routes />
-            </Content>
-            <GlobalStyle />
-          </Container>
-          <Player />
-        </Wrapper>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Wrapper>
+            <Container>
+              <Sidebar />
+              <Content>
+                <Header />
+                <Routes />
+              </Content>
+              <GlobalStyle />
+            </Container>
+            <Player />
+          </Wrapper>
+        </BrowserRouter>
+      </Provider>
     </Fragment>
   );
 }
